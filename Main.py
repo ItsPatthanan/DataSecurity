473f0c406b7ac3a4bfd78377c28d89e11c984c6e
K0BiMaChi <Patthanan_ja65@live.rmutl.ac.th>
2024-02-17 12:21:45 +0700

    replace cryptography

--- Main.py
+++ Main.py
@@ -1,132 +1,103 @@
+import os
 import PySimpleGUI as sg
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import padding
-from Crypto.Random import get_random_bytes
-
-# Function to create a centered text element
-def centered_text(text, size=(32, 1), font=('prompt 32 bold')):
-    return sg.Text(text, size=size, font=font, justification='center')
+from cryptography.fernet import Fernet
 
 
-def generate_aes_key():
-    return get_random_bytes(32)  # 256 bits
+def runEncrypted(input_file):
+    if not input_file:
+        sg.popup_error("Please choose a file for encryption.")
+        return None
 
-def encrypt_file(input_file, key):
-    iv = get_random_bytes(16)  # Generate a random IV for each encryption
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
-    encryptor = cipher.encryptor()
+    key = Fernet.generate_key()
+    cipher_suite = Fernet(key)
 
     with open(input_file, 'rb') as file:
         plaintext = file.read()
 
-    padder = padding.PKCS7(algorithms.AES.block_size).padder()
-    plaintext = padder.update(plaintext) + padder.finalize()
+    encrypted_data = cipher_suite.encrypt(plaintext)
 
-    ciphertext = encryptor.update(plaintext) + encryptor.finalize()
-    return ciphertext, iv
+    output_file = f"{os.path.splitext(input_file)[0]}_encrypted.txt"
+
+    with open(output_file, 'wb') as file:
+        file.write(encrypted_data)
+
+    key_file = f"{os.path.splitext(output_file)[0]}_key.txt"
+    with open(key_file, 'wb') as key_file:
+        key_file.write(key)
+
+    window['key_encrypted'].update(key)
+    window['encrypted_txt'].update(value=encrypted_data)
+    return key
 
 
-def decrypt_file(input_file, key, iv):
-    cipher = Cipher(algorithms.AES(key), modes.CBC(iv), backend=default_backend())
-    decryptor = cipher.decryptor()
+def runDecrypted(input_file, key_file):
+    if not input_file or not key_file:
+        sg.popup_error("Please choose a file for decryption and provide a decryption key.")
+        return None
+
+    with open(key_file, 'rb') as key_file:
+        key = key_file.read()
+
+    cipher_suite = Fernet(key)
 
     with open(input_file, 'rb') as file:
-        ciphertext = file.read()
+        encrypted_data = file.read()
 
-    plaintext = decryptor.update(ciphertext) + decryptor.finalize()
+    decrypted_data = cipher_suite.decrypt(encrypted_data)
 
-    unpadder = padding.PKCS7(algorithms.AES.block_size).unpadder()
-    unpadded_data = unpadder.update(plaintext) + unpadder.finalize()
+    output_file = f"{os.path.splitext(input_file)[0]}_decrypted.txt"
 
-    return unpadded_data
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(decrypted_data.decode('utf-8'))
+
+    window['decrypt_cipher_txt'].update(value=decrypted_data.decode("utf-8"))
+    return decrypted_data
 
 
-# Create two separate column layouts for each section
-layout1 = [
-    [sg.Column(
-        [
-            [centered_text('AES Encryption - K0BiMaChi')],
-            [sg.Input(), sg.FileBrowse(key='encryption_input')],
-            [sg.Button("Encryption")],
-            [sg.Text("KEY"), sg.Input(size=(65, 1), key='Encryption_key', readonly=True), sg.Button("Copy Key")],
-            [sg.Text("Encryption Output"),sg.Input(readonly=True,size=(50, 1), background_color='lightblue', key='encryption_output')],
+def reset():
+    window['BrowseInEn'].update('')
+    window['key_encrypted'].update('')
+    window['encrypted_txt'].update('')
+    window['BrowseInDe'].update('')
+    window['key_decrypted'].update('')
+    window['decrypt_cipher_txt'].update('')
 
-        ],
-        element_justification="center",
-    )]
+
+# design layout
+sg.theme('Dark Blue17')
+Font = ("prompt", 12)
+FontHead = ("prompt", 16, "bold")
+layout = [
+    [sg.Frame("AES Encryption - K0BiMaChi", font=FontHead, layout=[
+        [sg.Text("Plaintext", font=Font), sg.Input(readonly=True, key='BrowseInEn'), sg.FileBrowse(key='encryption_input', font=Font), sg.Button("Encrypted", font=Font)],
+        [sg.Text("Key Encryption", font=Font), sg.Input(size=(50, 1), key='key_encrypted', readonly=True)],
+        [sg.Text("Encryption output", font=Font), sg.Multiline(size=(60, 4), key='encrypted_txt')]
+    ])],
+    [sg.Frame("AES Decryption - K0BiMaChi", font=FontHead, layout=[
+        [sg.Text("Cipher ->", font=Font), sg.Input(readonly=True, key='BrowseInDe'), sg.FileBrowse(key='decryption_input', font=Font), sg.Button("Decrypted", font=Font)],
+        [sg.Text("Key Decryption", font=Font), sg.Input(size=(50, 1), key='key_decrypted', readonly=True), sg.FileBrowse(key='key_input', font=Font)],
+        [sg.Text("Decryption output", font=Font), sg.Multiline(size=(60, 4), key='decrypt_cipher_txt')]
+    ])],
+    [sg.Text("", size=(60, 1), justification="right"), sg.Button("Reset", font=Font), sg.Button("Exit", font=Font)]
+
 ]
 
-layout2 = [
-    [sg.Column(
-        [
-            [centered_text('AES Decryption - K0BiMaChi')],
-            [sg.Input(), sg.FileBrowse(key='decryption_input')],
-            [sg.Text("KEY"),sg.Input(key='Decryption_key', readonly=True,size=(65, 1)), sg.Button("Paste key")],
-            [sg.Button("Decryption")],
-            [sg.Text("Decryption Output"),sg.Input(readonly=True,size=(50, 1), background_color='lightblue', key='decryption_output')],
-        ],
-        element_justification="center",
-    )]
-]
 
-# Add an empty row with a border between layout1 and layout2
-separator = [[sg.Canvas(background_color='lightblue', size=(900, 2))]]
-
-# Combine layout1, separator, and layout2
-final_layout = layout1 + separator + layout2
-
-# Create the window
-window = sg.Window('AES Encryption/Decryption - K0BiMaChi', final_layout)
-
-key_to_copy = b''  # Variable to store the generated key
+window = sg.Window('AES Encryption/Decryption - K0BiMaChi ', layout, resizable=True)
 
 while True:
     event, values = window.read()
 
-    # Handle events
-    if event == sg.WIN_CLOSED or event == 'Cancel':
+    if event == sg.WIN_CLOSED or event == 'Exit':
         break
-    elif event == 'Encryption':
-        key = generate_aes_key()
-        window['Encryption_key'].update(value=key.hex())
+    elif event == 'Encrypted':
         input_file = values['encryption_input']
-        ciphertext, iv = encrypt_file(input_file, key)
-        window['encryption_output'].update(value='File encrypted successfully.')
-        encrypted_file = input_file + '_encrypted.txt'
-        with open(encrypted_file, 'wb') as file:
-            file.write(iv)  # เขียน IV ลงในไฟล์
-            file.write(ciphertext)
-        window['encryption_output'].update(value=f'{encrypted_file}')
-
-    elif event == 'Copy Key':
-        key_to_copy = key.hex()
-
-    elif event == 'Decryption':
-
+        runEncrypted(input_file)
+    elif event == 'Decrypted':
         input_file = values['decryption_input']
-
-        key = bytes.fromhex(values['Decryption_key'])
-
-        with open(input_file, 'rb') as file:
-
-            iv = file.read(16)  # Read the IV from the file
-
-            decrypted_data = decrypt_file(input_file, key, iv)
-
-        window['decryption_output'].update(value=decrypted_data)
-
-        decrypted_filename = input_file.rsplit('.', 1)[0]  # Remove the last extension
-
-        decrypted_filename = f'{decrypted_filename}_decrypted.txt'
-
-        with open(decrypted_filename, 'wb') as file:
-
-            file.write(decrypted_data)
-
-        window['decryption_output'].update(value=f'{decrypted_filename}')
-
-    elif event == 'Paste key':
-        window['Decryption_key'].update(value=key_to_copy)
+        key_file = values['key_input']
+        decrypted_data = runDecrypted(input_file, key_file)
+    elif event == 'Reset':
+        reset()
 
 window.close()
